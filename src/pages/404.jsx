1f9de534fb7755c98155bc58b715ce4d3856a658
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2022-02-18 11:42:55 +0700

    fix: fix 404

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -27,15 +27,11 @@ const codeStyles = {
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main style={pageStyles} className="bg-white h-screen">
       <title>Not found</title>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
+        Sorry we couldn’t find what you were looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
